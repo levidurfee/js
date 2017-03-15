2f7eb38e841c4a5c1c361ccc1ffeebe04ddb64fa
Levi Durfee <levi.durfee@gmail.com>
2017-03-15 19:30:23 -0400

    play around with mongo

---
+++ test2.js
@@ -0,0 +1,44 @@
+fs = require('fs');
+prompt = require('prompt');
+
+mongoClient = require('mongodb').MongoClient;
+console.log('\nLoading Config...\n');
+content = fs.readFileSync('creds.json');
+jsonConfig = JSON.parse(content);
+console.log('\nConfig Loaded.\n');
+
+prompt.start();
+
+mongoClient.connect('mongodb://'
+  + jsonConfig.username + ':'
+  + jsonConfig.password + '@'
+  + jsonConfig.hostname + ':'
+  + jsonConfig.port + '/'
+  + jsonConfig.database,
+  function(error, db) {
+    if(!error) {
+      console.log('Connected\n');
+    }
+    db.collection('test', function(err, collection) {});
+    collection = db.collection('test');
+
+    /**
+     * get info from user
+     */
+    function getInfo() {
+      prompt.get(['username', 'email'], function(err, result) {
+        if(result.username == 'done') {
+          process.exit(1);
+        } else {
+          doc = {
+            'username': result.username,
+            'email': result.email,
+          };
+          collection.insert(doc);
+          getInfo();
+        }
+      });
+    };
+
+    getInfo();
+});
