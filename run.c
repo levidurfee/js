dc5e07623e8f94b1aafe49653ae1c402a3344fcb
Levi Durfee <levi.durfee@gmail.com>
2017-03-23 19:41:19 -0400

    change indent to spaces

--- run.c
+++ run.c
@@ -3,10 +3,10 @@
 #include <string.h>
 
 int main(int argc, char *argv[]) {
-	char command[512];
-	sprintf(command, "./fail %s %s", argv[1], argv[2]);
-	printf("Running...\n");
-	printf("%s %s\n", argv[1], argv[2]);
-	int i = system(command);
-	return 0;
+    char command[512];
+    sprintf(command, "./fail %s %s", argv[1], argv[2]);
+    printf("Running...\n");
+    printf("%s %s\n", argv[1], argv[2]);
+    int i = system(command);
+    return 0;
 }
